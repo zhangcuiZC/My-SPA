7d60b1f7e6e4fa0e22e57161e5eb4db7eed7a644
ZC <zczhangcui@foxmail.com>
2017-02-23 20:16:57 +0800

    day4

--- js/spa.js
+++ js/spa.js
@@ -1,8 +1,11 @@
 require("../css/spa.css");
 var spa_shell=require("./spa.shell.js");
+var spa_model=require("./spa.model.js");
 
 module.exports = (function(){
+	'use strict';
 	var initModule = function($container){
+		spa_model.initModule();
 		spa_shell.initModule($container);
 	};
 
