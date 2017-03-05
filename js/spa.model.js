1d6c8b57a84539fa62e8d4dfe0ed92bdcfb57fee
ZC <zczhangcui@foxmail.com>
2017-03-05 20:56:06 +0800

    update

--- js/spa.model.js
+++ js/spa.model.js
@@ -52,6 +52,7 @@ module.exports = (function(){
 		stateMap.user.id = user_map._id;
 		stateMap.user.css_map = user_map.css_map;
 		stateMap.people_cid_map[user_map._id] = stateMap.user;
+		chat.join();
 
 		$.gevent.publish('spa-login',[stateMap.user]);
 	};
@@ -132,6 +133,7 @@ module.exports = (function(){
 		logout = function(){
 			var is_removed, user = stateMap.user;
 
+			chat._leave();
 			is_removed = removePerson(user);
 			stateMap.user = stateMap.anon_user;
 
@@ -149,16 +151,17 @@ module.exports = (function(){
 	}());
 
 	chat = (function(){
-		var _publish_listchange,
-			_update_list,
-			_leave_chat,
-			join_chat;
+		var _publish_listchange, _publish_updatechat,
+			_update_list, _leave_chat,
+			get_chatee, join_chat, send_msg, set_chatee,
+			chatee = null;
 
 		_update_list = function(arg_list){
 			var i,
 				person_map,
 				make_person_map,
-				people_list = arg_list[0];
+				people_list = arg_list[0],
+				is_chatee_online = false;
 
 			clearPeopleDb();
 
@@ -182,10 +185,18 @@ module.exports = (function(){
 					name : person_map.name
 				};
 
+				if (chatee && chatee.id === make_person_map.id) {
+					is_chatee_online = true;
+				}
+
 				makePerson(make_person_map);
 			}
 
 			stateMap.people_db.sort('name');
+
+			if (chatee && !is_chatee_online) {
+				set_chatee('');
+			}
 		};
 
 		_publish_listchange = function(arg_list){
@@ -193,12 +204,29 @@ module.exports = (function(){
 			$.gevent.publish('spa-listchange',[arg_list]);
 		};
 
+		_publish_updatechat = function(arg_list){
+			var msg_map = arg_list[0];
+
+			if (!chatee) {
+				set_chatee(msg_map.sender_id);
+			}else if (msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id) {
+				set_chatee(msg_map.sender_id);
+			}
+
+			$.gevent.publish('spa-updatechat', [msg_map]);
+		};
+
 		_leave_chat = function(){
 			var sio = isFakeData ? spa_fake.mockSio : spa_data.getSio();
+			chatee = null;
 			stateMap.is_connected = false;
 			if (sio) {
 				sio.emit('leavechat');
 			}
+		};
+
+		get_chatee = function(){
+			return chatee;
 		};
 
 		join_chat = function(){
@@ -215,13 +243,57 @@ module.exports = (function(){
 
 			sio = isFakeData ? spa_fake.mockSio : spa_data.getSio();
 			sio.on('listchange', _publish_listchange);
+			sio.on('updatechat', _publish_updatechat);
 			stateMap.is_connected = true;
+			return true;
+		};
+
+		send_msg = function(msg_text){
+			var msg_map,
+				sio = isFakeData ? spa_fake.mockSio : spa_data.getSio();
+
+			if (!sio) {
+				return false;
+			}
+			if (!(stateMap.user && chatee)) {
+				return false;
+			}
+
+			msg_map = {
+				dest_id : chatee.id,
+				dest_name : chatee.name,
+				sender_id : stateMap.user.id,
+				msg_text : msg_text
+			};
+
+			_publish_updatechat([msg_map]);
+			sio.emit('updatechat', msg_map);
+			return true;
+		};
+
+		set_chatee = function(person_id){
+			var new_chatee;
+			new_chatee = stateMap.people_cid_map[person_id];
+
+			if (new_chatee) {
+				if (chatee && chatee.id === new_chatee.id) {
+					return false;
+				}
+			}else {
+				new_chatee = null;
+			}
+
+			$.gevent.publish('spa-setchatee', {old_chatee : chatee, new_chatee : new_chatee});
+			chatee = new_chatee;
 			return true;
 		};
 
 		return {
 			_leave : _leave_chat,
-			join : join_chat
+			get_chatee : get_chatee,
+			join : join_chat,
+			send_msg : send_msg,
+			set_chatee : set_chatee
 		};
 	}());
 		
