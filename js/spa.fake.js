c8cecea1bf79a0a32e998e0c5ad4664fbdf81140
ZC <zczhangcui@foxmail.com>
2017-03-08 21:36:30 +0800

    update

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -60,7 +60,7 @@ module.exports = (function(){
 		};
 
 		emit_sio = function(msg_type, data){
-			var person_map;
+			var person_map, i;
 			if (msg_type === 'adduser' && callback_map.userupdate) {
 				setTimeout(function(){
 					person_map = {
@@ -94,6 +94,16 @@ module.exports = (function(){
 					listchange_idto = undefined;
 				}
 				send_listchange();
+			}
+
+			if (msg_type === 'updateavatar' && callback_map.listchange) {
+				for(i = 0; i < peopleList.length; i++){
+					if (peopleList[i]._id === data.person_id) {
+						peopleList[i].css_map = data.css_map;
+						break;
+					}
+				}
+				callback_map.listchange([peopleList]);
 			}
 		};
 
